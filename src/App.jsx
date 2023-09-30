cb1d287663f1f081e950311e4aa0a0aa27af7b33
Nick Fong <nicholasfong1120@gmail.com>
2023-09-30 16:15:09 -0700

    compressed all images

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import "./App.css";
+import Landing from "./pages/Landing"
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  <Landing/>
 }
 
 export default App;
